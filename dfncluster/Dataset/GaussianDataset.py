7615ec73c0904cc054f35952e9f5dd6f8e874dc8
bbradt <bbradt@gmail.com>
2020-03-03 12:08:17 -0500

    trying to fix submodules

---
+++ dfncluster/Dataset/GaussianDataset.py
@@ -0,0 +1,30 @@
+from data import Dataset
+
+
+class GaussianDataset(Dataset):
+    def __init__(self, parameters=[dict(sigma=1, mu=-1, N=1024), dict(sigma=1, mu=1, N=1024)], num_features=2):
+        """
+             args:
+                filename	string	name of the CSV to load
+        """
+        super(GaussianDataset, self).__init__(
+            parameters=parameters, num_features=num_features)
+
+    def generate(self,  **kwargs):
+        parameters = kwargs['parameters']
+        num_features = kwargs['num_features']
+        features = None
+        labels = None
+        for label, parameter_set in enumerate(parameters):
+            N = parameter_set['N']
+            i_features = np.random.normal(
+                size=(N, num_features), scale=parameter_set['sigma'], loc=parameter_set['mu'])
+            i_labels = np.ones((N, 1)) * label
+            if features is None or labels is None:
+                features = i_features
+                labels = i_labels
+            else:
+                features = np.vstack((features, i_features))
+                labels = np.vstack((labels, i_labels))
+        print(labels.shape, features.shape)
+        return features, labels.flatten()
---
+++ dfncluster/Dataset/__init__.py
@@ -0,0 +1,7 @@
+from .Dataset import Dataset
+from .GaussianDataset import GaussianDataset
+from .ImageDataset import ImageDataset
+from .NiftiDataset import NiftiDataset
+from .SklearnDataset import SklearnDataset
+
+__all__ = ["Dataset", "GaussianDataset", "ImageDataset", "NiftiDataset", "SklearnDataset"]
