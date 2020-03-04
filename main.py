04d7a4ecab12d4b3c20d2cc86bd28d9024203743
Bradley Thomas Baker <bbaker43@gsu.edu>
2020-03-04 14:21:40 -0500

    working polyssifier

--- main.py
+++ main.py
@@ -3,30 +3,46 @@ from dfncluster.Clusterer import KMeansClusterer
 from dfncluster.dFNC import dFNC
 from dfncluster.Classifiers import Polyssifier
 
-# Parameters for KMeans
-kmeans_params = dict(
-    init='k-means++',
-    n_init=100,
-    tol=1e-6,
-    n_clusters=5,
-    metrics=['silhouette'],
-    verbose=0
-)
+import numpy as np
 
-# Load the Data Set
-fbirn_data = FNCDataset.load('data/FNCDatasets/FbirnFNC/fbirn_fnc.npy')
-subject_data, subject_labels = fbirn_data.get_subjects()
+if __name__=='__main__':
 
-# Create the dFNC Runner
-dfnc = dFNC(dataset=fbirn_data, clusterer=KMeansClusterer)
+    # Parameters for KMeans
+    kmeans_params = dict(
+        init='k-means++',
+        n_init=100,
+        tol=1e-6,
+        n_clusters=5,
+        metrics=['silhouette'],
+        verbose=0
+    )
 
-# Run it, passing KMeans Params
-results, assignments = dfnc.run(**kmeans_params)
+    # Load the Data Set
+    fbirn_data = FNCDataset.load('data/FNCDatasets/FbirnFNC/fbirn_fnc.npy')
+    subject_data, subject_labels = fbirn_data.get_subjects()
 
-# Print results
-print(results)
-print(assignments, assignments.shape, fbirn_data.labels.shape)
+    # Create the dFNC Runner
+    dfnc = dFNC(dataset=fbirn_data, clusterer=KMeansClusterer)
 
-poly = Polyssifier(assignments, subject_labels)
-poly.build()
-poly.run()
+    # Run it, passing KMeans Params
+    results, assignments = dfnc.run(**kmeans_params)
+
+    # Print results
+    print(results)
+    print(assignments, assignments.shape, fbirn_data.labels.shape)
+
+    poly = Polyssifier(assignments,
+                       subject_labels,
+                       path='results/polyssifier',
+                       project_name='KMeans',
+                       concurrency=1)
+    poly.build()
+    poly.run()
+
+    poly = Polyssifier(subject_data.reshape(subject_data.shape[0],np.prod(subject_data.shape[1:])),
+                       subject_labels,
+                       path='results/polyssifier',
+                       project_name='FNCOnly',
+                       concurrency=1)
+    poly.build()
+    poly.run()
