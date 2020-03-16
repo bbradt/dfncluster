3cf31e3d7691845cea6713bc474a441cd910048b
bbradt <bbradt@gmail.com>
2020-03-16 13:02:46 -0400

    all documentation for Datasets finished

--- dfncluster/Dataset/NiftiDataset.py
+++ dfncluster/Dataset/NiftiDataset.py
@@ -3,10 +3,31 @@ from dfncluster.Dataset import FileFeatureDataset
 
 
 def load_wrapper(filename):
+    """wrap the nibabel loader, returning data directly
+    """
     return nib.load(filename).dataobj
 
 
 class NiftiDataset(FileFeatureDataset):
+    """
+        NiftiDataset class. Extends the FileFeatureDataset class. See the docstring for Dataset for inherited fields/functions.
+        Generates a dataset based on a CSV or TSV file, where features are given as .nii or .nii.gz files.
+        Each NiftiDataset has:
+            All inherited fields from FileFeatureDataset
+        Each Dataset does:
+            All inherited functions from FileFeatureDataset
+    """
 
     def __init__(self, **kwargs):
+        """
+            Constructor for NiftiDataset.
+            Usage:
+                dataset = NiftiDataset(shuffle=True,feature_columns=['feature_1','feature_2'],label_columns=['label'],**kwargs)
+            Kwargs:
+                See documnetation for FileFeatureDataset
+            Args:
+                -
+            Return:
+                Instantiated NiftiDataset Object
+        """
         super(NiftiDataset, self).__init__(loader=load_wrapper, **kwargs)
--- dfncluster/Dataset/SklearnDataset.py
+++ dfncluster/Dataset/SklearnDataset.py
@@ -9,7 +9,37 @@ DEFAULT_DATASET = 'make_moons'
 
 
 class SklearnDataset(Dataset):
+    """
+        SklearnDataset class. Wraps sklearn data loaders, and adds our 
+            bundled in Dataset functionality.
+        Each Dataset has:
+            All fields from the Dataset superclass
+        Each Dataset does:
+            All functions from Dataset superclass
+            ---
+            function name   |   return type         |   args                |   kwargs              |   description
+            ----------------|-----------------------|-----------------------|-----------------------|--------------------
+            __init___       |   Dataset             |   self                |   shuffle, **kwargs   |   class constructor
+            generate        |   (ndarray, ndarray)  |   self                |   **kwargs            |   generates the dataset (features, labels)
+            resolve_kwargs  |   dict                | dataset_method,kwargs |   -                   |   sklearn only supports some kwargs, so we need to match those.
+    """
+
     def __init__(self, dataset_name=DEFAULT_DATASET, dataset_method=None, **kwargs):
+        """
+            Constructor for SklearnDataset super-class. Generally should not be instantiated.
+            Usage:
+                dataset = SklearnDataset(dataset_name='iris',**kwargs)
+            Kwargs:
+                keyword         |   type        |   default     | Description                                    
+                ----------------|---------------|---------------|-------------------
+                dataset_name    |   str         |   'moons'     | the name of the dataset to loads, the name of the function to use
+                dataset_method  |   function    |   None        | function handle for dataset loading directly.    
+                NOTE: Other kwargs are passed to the self.generate function
+            Args:
+                -
+            Return:
+                Instantiated Dataset Object
+        """
         if dataset_method is None:
             if dataset_name not in SKLEARN_DATASETS.keys():
                 raise(Exception("The Dataset %s is not supported in sklearn" % dataset_name))
@@ -25,6 +55,9 @@ class SklearnDataset(Dataset):
         return {k: v for k, v in kwargs.items() if k in inspect.getfullargspec(dataset_method).args}
 
     def generate(self, **kwargs):
+        """Wrap the sklearn dataset generation function.
+        NOTE: All kwargs passed here are passed to that sklearn method.
+        """
         dataset_method = kwargs.pop('dataset_method')
         X, y = dataset_method(**kwargs)
         return X, y
