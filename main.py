6b3ef300abb7eacac6737dcbc8adbbfc2fb53a6f
Anish Khazane <akkhazan@berkeley.edu>
2020-03-25 12:20:18 -0700

    integrating DBSCAN

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 from dfncluster.Dataset import FNCDataset
 from dfncluster.Clusterer import KMeansClusterer
+from dfncluster.Clusterer import DBSCANClusterer
 from dfncluster.dFNC import dFNC
 from dfncluster.Classifiers import Polyssifier
 
 import os
 import numpy as np
+import pdb
 
 if __name__=='__main__':
-
+    pdb.set_trace()
     # Parameters for KMeans
-    kmeans_params = dict(
-        init='k-means++',
-        n_init=100,
-        tol=1e-6,
-        n_clusters=5,
-        metrics=['silhouette'],
-        verbose=0
+
+    pdb.set_trace() 
+    # kmeans_params = dict(
+    #     init='k-means++',
+    #     n_init=100,
+    #     tol=1e-6,
+    #     n_clusters=5,
+    #     metrics=['silhouette'],
+    #     verbose=0
+    # )
+
+    dbscan_params = dict(
+        eps=0.5,
+        min_samples=5,
     )
 
+
     # Load the Data Set
-    fbirn_data = FNCDataset.load('data/FNCDatasets/OmegaSim/omega_sim.npy')
+    # fbirn_data = FNCDataset.load('data/FNCDatasets/OmegaSim/omega_sim.npy')
+    pdb.set_trace()
+    fbirn_data = np.load('data/SklearnDatasets/Moons/Moons.npy')
     subject_data, subject_labels = fbirn_data.get_subjects()
 
+    pdb.set_trace()
+
     # Create the dFNC Runner
-    dfnc = dFNC(dataset=fbirn_data, clusterer=KMeansClusterer)
+    dfnc = dFNC(dataset=fbirn_data, clusterer=DBSCANClusterer)
 
     # Run it, passing KMeans Params
     results, assignments = dfnc.run(**kmeans_params)
