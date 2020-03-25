16f7b4bfdf5bfd8e4adfcc13edbf59bb72f1f778
bbradt <bbradt@gmail.com>
2020-03-25 18:26:50 -0400

    removed dfnc test...

--- tests/dfncluster/dFNC/test_dFNC.py
+++ tests/dfncluster/dFNC/test_dFNC.py
@@ -1,19 +1,1 @@
 # TODO: These tests are incomplete
-import os
-import numpy as np
-from copy import deepcopy
-import scipy.io as sio
-from unittest.mock import patch
-from tempfile import TemporaryDirectory, TemporaryFile
-from dfncluster.Dataset import FNCDataset
-from dfncluster.Dataset.MatDataset import load_wrapper
-
-PARAMETERS = dict(
-    shuffle_instances=True,
-    filename='data.csv',
-    feature_columns=['feature'],
-    label_columns=['label'],
-    pre_shuffle=False
-)
-TEST_CLASS = FNCDataset
-
