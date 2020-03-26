4a35faf6cb58a62197dc29b94d3100f243c4a199
Eric Martin <ericmartin827@gmail.com>
2020-03-25 21:13:41 -0400

    added test_BayesianGMMExample.py, ran pytests

--- dfncluster/Clusterer/__init__.py
+++ dfncluster/Clusterer/__init__.py
@@ -1,6 +1,6 @@
 from .Clusterer import Clusterer
 from .KMeansClusterer import KMeansClusterer
-from .GMMClusterer import GMMCluster
+from .GMMClusterer import GMMClusterer
 from .BayesianGMMClusterer import BayesianGMMClusterer
 
 __all__=['Clusterer', 'KMeansClusterer', 'GMMClusterer', 'BayesianGMMClusterer']
