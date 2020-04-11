c839b4f36b80f88791d32af57eb9dc6d667ed645
Eric Martin <ericmartin827@gmail.com>
2020-04-11 16:17:43 -0400

    Visualisation/Plots.py: updated and testest for generating multipel ptest reuslts

--- dfncluster/Clusterer/Visualisation/Plots.py
+++ dfncluster/Clusterer/Visualisation/Plots.py
@@ -33,3 +33,52 @@ def plot_curve_compare(x, y1, y2, title='Ys over X',
     plt.grid(grid_enable)
     if filename_path is not None:
         plt.savefig(filename_path)
+
+
+def plot_multiple_ptest_results(df, nrows, ncols, title, figsize=None, filename=None):
+
+    # Initialize Figure
+    plt.style.use('seaborn-darkgrid')
+
+    # Create a color-palette
+    palette = ['b', 'g', 'r', 'c', 'm', 'k']
+
+    if figsize is not None:
+        plt.figure(figsize=figsize)
+
+    subplot_num = 0
+    for result in df.drop('time_window', axis=1):
+        subplot_num += 1
+        plt.subplot(nrows, ncols, subplot_num)
+
+        # plot all results in grey-scale
+        for ele in df.drop('time_window', axis=1):
+            plt.plot(
+                df['time_window'], df[ele],
+                marker='', color='grey',
+                linewidth=0.6, alpha=0.3)
+
+        # plot specific algorithm results in bold color
+        plt.plot(df['time_window'], df[result],
+                 color=palette[subplot_num % len(palette)],
+                 label=result,
+                 linewidth=2.4, alpha=0.9)
+        plt.title(result, loc='left',
+                  color=palette[subplot_num % len(palette)],
+                  fontsize=12, fontweight=0)
+
+        if (subplot_num - 1) % ncols == 0:
+            plt.yticks([0, 1])
+            plt.ylabel('Significant')
+        else:
+            plt.tick_params(labelleft=False)
+
+        if subplot_num <= (nrows - 1) * ncols:
+            plt.tick_params(labelbottom=False)
+        else:
+            plt.xlabel('Time')
+
+    plt.suptitle(title, fontsize=20)
+
+    if filename is not None:
+        plt.savefig(filename)
--- tests/dfncluster/Clusterer/Visualisation/test_Plots.py
+++ tests/dfncluster/Clusterer/Visualisation/test_Plots.py
@@ -1,6 +1,9 @@
 import os
 import numpy as np
-from dfncluster.Clusterer.Visualisation.Plots import plot_curve, plot_curve_compare
+import pandas as pd
+from dfncluster.Clusterer.Metrics.TTest import t_test
+from dfncluster.Clusterer.Visualisation.Plots import \
+    plot_curve, plot_curve_compare, plot_multiple_ptest_results
 
 
 def test_plot_curve():
@@ -21,3 +24,48 @@ def test_multiple_2d_plot():
         assert(not f.closed)
     assert(f.closed)
     os.remove(target_file)
+
+
+def test_plot_multiple_ptest_results():
+    # stage prepared assignments and label data representative of dFNC results
+    assignments = np.loadtxt('assignments.csv')
+    subject_labels = np.loadtxt('subject_labels.csv')
+
+    # can change significance levels of test via p_level
+    sig_levels = t_test(assignments, subject_labels, p_level=0.05)
+
+    df = pd.DataFrame({
+
+        # required argument to specify x-axis used across all subplots
+        'time_window': np.arange(sig_levels.size),
+
+        # after specifying time window, add as many as you want
+        # going to use same data across all 'clustering algorithms'
+        # for simplicity
+        'kmeans': sig_levels,
+        'gmm': sig_levels,
+        'bayes': sig_levels,
+        'dbscan': sig_levels,
+        'foo': sig_levels,
+        'bar': sig_levels,
+        'dog': sig_levels,
+        'oracle': sig_levels,
+        'pokemon': sig_levels,
+        'dragon': sig_levels,
+        'bird': sig_levels,
+        'rockets': sig_levels,
+    })
+
+    # plot the 12 subplots into a 3, 4 layout and specify figsize to increase size
+    # and avoid tight subplot layouts
+    # call must provide these arguments in order to allow for arbitrary amount of
+    # clustering results
+    plot_multiple_ptest_results(
+        df, nrows=3, ncols=4,
+        title='Example Plot Title', figsize=(16, 10),
+        filename='test.png')
+
+    with open('test.png') as f:
+        assert(not f.closed)
+    assert(f.closed)
+    os.remove('test.png')
