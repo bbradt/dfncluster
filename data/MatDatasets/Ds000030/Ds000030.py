3126d97ad65cd5972c9a4d2b8395d8f1fc9cffd9
bbradt <bbaker43@gsu.edu>
2020-04-13 10:39:27 -0400

    added Ds000030

---
+++ data/MatDatasets/Ds000030/Ds000030.py
@@ -0,0 +1,16 @@
+import json
+from dfncluster.Dataset import MatDataset
+
+
+class Ds000030:
+    @staticmethod
+    def make():
+        dataset = MatDataset(filename="results/ica/ds000030/derivatives/data.csv",
+                             feature_columns=['filename'],
+                             label_columns=['diagnosis'])
+        dataset.save('data/MatDatasets/Ds000030/ds000030')
+        return dataset
+
+
+if __name__ == '__main__':
+    dataset = FbirnTC.make()
